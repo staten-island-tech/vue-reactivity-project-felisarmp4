1e4cd013517ab072045282e85b03f6977c318020
felisarmp4 <felisayereyes@gmail.com>
2026-02-27 14:03:49 -0500

    asdas

---
+++ vue-project/src/components/foodbutton.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+<h2>{{ animal.name }}</h2>
+<h2>{{ animal.species }}</h2>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+   food : {
+        type: Object,
+        required: true
+    }
+})
+</script>
+
+<style scoped>
+    div{
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: center;
+        height: 200px;
+        width: 150px;
+        border: 3px solid black;
+    }
+</style>
